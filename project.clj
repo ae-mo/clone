f94b2b8b35e81d8adbf1b3bbcd469addb6b2e06b
amorcian <morciano.andrea92@gmail.com>
2019-10-20 15:24:48 +0200

    Fixed app url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clone "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "https://clone-persona.heroku.com"
+  :url "https://clone-persona.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
